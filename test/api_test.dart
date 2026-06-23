1a03ff34953d97fe500cc0f988bbe0f14c9328fe
Diego <dl7designer@gmail.com>
2026-06-23 00:00:37 -0300

    refactor: alterado o arquivo test/api_test

--- test/api_test.dart
+++ test/api_test.dart
@@ -3,12 +3,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
   test('Deve buscar a lista de produtos do Supabase com sucesso', () async {
-    // Inicializa o Supabase desativando o armazenamento em cache local do celular
     await Supabase.initialize(
       url: "https://ucdecpenkxmuuwpmmbgt.supabase.co",
       publishableKey: 'sb_publishable_N_m5Da8h_8SVTl-jOKBLxw_FqTa90VV',
       authOptions: const FlutterAuthClientOptions(
-        localStorage: EmptyLocalStorage(), // <-- ISSO resolve o erro no teste!
+        localStorage: EmptyLocalStorage(),
       ),
     );
 
