47f0c25e391ad3cbbff6c1ad1bf133a6b357bcd6
Alexandre Rodrigues <aleeh.rodrigues92@gmail.com>
2026-07-01 16:08:04 -0300

    fix: update firebase options and product detail store generated file

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ import 'package:growstore/features/auth/pages/login_page.dart';
 import 'package:growstore/features/cart/pages/cart_page.dart';
 import 'package:growstore/features/checkout/pages/checkout_page.dart';
 import 'package:growstore/features/orders/pages/order_detail_page.dart';
-import 'firebase_options.dart';
-
+import 'package:growstore/firebase_options.dart';
 import 'package:get_it/get_it.dart';
 
 import 'package:growstore/core/theme/theme_mode_controller.dart';
