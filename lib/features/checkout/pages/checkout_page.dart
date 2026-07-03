7b037abfa94b78a35ed53d24c86b72b3eea0b278
JhonnyAraujo <jhonny.a.santana2002@gmail.com>
2026-07-02 23:49:33 -0300

    refactor: remove unused import from checkout_page.dart

--- lib/features/checkout/pages/checkout_page.dart
+++ lib/features/checkout/pages/checkout_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
