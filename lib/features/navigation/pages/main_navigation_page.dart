34a0b3257eea00e1e80fabbcf406fef7ba049ee8
Diego <dl7designer@gmail.com>
2026-07-02 23:09:32 -0300

    refactor: import removido

--- lib/features/navigation/pages/main_navigation_page.dart
+++ lib/features/navigation/pages/main_navigation_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:growstore/features/cart/pages/cart_page.dart';
